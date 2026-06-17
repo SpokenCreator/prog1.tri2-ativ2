f8e739a40aac58d7fefe441f65c887ed2857c922
Igor A. Rodrigues <igoraragaorodrigues10@gmail.com>
2026-06-17 16:41:35 -0300

    Add files via upload

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,67 @@
-const server = Bun.serve({
-    routes: {
-        "/api-debugger": Bun.file(`public/api-debugger.html`),
-        "/test": {
-            GET: () => Response.json({time: Date.now() }),
-            PUT: () => Response.json({time: Date.now() }),
-            POST: () => Response.json({time: Date.now() }),
-            DELETE: () => Response.json({time: Date.now() }),
-        }
-    },
-    fetch() {
-        return new Response("Not Found", {status: 404})
-    }
-})
+import TodoListClass, { Item } from "./core"
+const todolist = new TodoListClass("todolist.json")
 
-console.log(`⚡ http://localhost:${server.port}`)
+async function testRoute(req: Bun.BunRequest) {
+  return Response.json({
+    method: req.method,
+    time: new Date().toLocaleString('pt-BR'),
+    body: await req.body?.text(),
+  });
+}
+
+const server = Bun.serve({
+  port: 3000,
+  routes: {
+    '/api-debugger': (req) => new Response(Bun.file('./public/api-debugger.html')),
+    '/test': {
+      GET: testRoute,
+      POST: testRoute,
+      PUT: testRoute,
+      DELETE: testRoute,
+      PATCH: testRoute,
+      OPTIONS: testRoute,
+    },
+    '/todo': {
+      GET: async () => {
+        const items = await todolist.getItems()
+        return Response.json(items)
+      },
+
+      POST: async (req) => {
+        let data
+  
+        try {
+          data = await req.body?.json()
+        } catch(e) {
+          return new Response('json inválido', { status: 400 })
+        }
+
+        if (!data?.title) 
+          return new Response('É preciso informar title', { status: 400 })
+
+        try {
+          await todolist.addItem(new Item(data.title))
+        } catch (error) {
+          return new Response('Erro ao adicionar item', { status: 500 })
+        }
+
+        return new Response('Created', { status: 201 })
+      }
+    },
+    '/todo/:index': {
+      DELETE: (req) => {
+        const strIndex = req.params.index
+        const index = parseInt(strIndex)
+        if (isNaN(index)) 
+          return new Response('/todo/:index index precisa ser um número inteiro', { status: 400 })
+        todolist.removeItem(index)
+        return new Response(`Item do index ${index} removido.`)
+      }
+    }
+  },
+  fetch(req) {
+    return new Response("Not Found", { status: 404 });
+  },
+});
+
+console.log(`Server running at http://localhost:${server.port}`);
